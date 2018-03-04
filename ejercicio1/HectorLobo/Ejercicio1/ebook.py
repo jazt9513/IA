23a8e215d0d41ccc8a0259cb650218f8b5e3584a
Hectorlobo10000 <hector.lobo10000@gmail.com>
2018-03-03 22:53:03 -0600

    arreglado funciona 100%

--- ejercicio1/HectorLobo/Ejercicio1/ebook.py
+++ ejercicio1/HectorLobo/Ejercicio1/ebook.py
@@ -28,7 +28,8 @@ class EBook:
     {'nieve': 4, 'el': 2, 'Le': 1, 'd\xc3\xada': 1, 'despist\xc3\xb3': 1, 'En': 1, 'tener': 1, 'dejando': 1, 'copos': 1, 'mientras': 1, 'en': 1, 'momento': 1, 'ca\xc3\xadan': 1, 'tal': 1, 'Un': 1, 'cabo': 1, 'm\xc3\xa1s': 1, 'forma': 1, 'le': 1, 'roja': 1, 'la': 8, 'blanca': 2, 'luz': 1, 'De': 1, 'dedo': 1, 'hija': 1, 'aunque': 1, 'as\xc3\xad': 1, 'los': 2, 'ese': 1, 'un': 2, 'c\xc3\xb3mo': 1, 'sobre': 1, 'ni\xc3\xb1a': 1, 'desear\xc3\xada': 1, 'muerte': 1, 'deseo': 1, 'cos\xc3\xada': 1, 'caer': 1, 'tiempo': 1, 'y': 4, 'de': 7, 'miraba': 1, 'cautivaron': 1, 'cabellos': 2, 'bell\xc3\xadsima': 1, 'negros': 1, 'Blancanieves': 1, 'como': 6, 'nombre': 1, 'gotas': 1, 'nacimiento': 1, 'a': 2, 'pinch\xc3\xb3': 1, 'dio': 1, 'sangre': 3, 'sonrosada': 2, 'invierno': 1, 'una': 2, 'Al': 1, 'su': 3, 'cumpli\xc3\xb3': 1, 'supuso': 1, 'pens\xc3\xb3:': 1, 'se': 3, 'madre': 1, '\xc3\xa9bano': 2, 'que': 1, 'tres': 1, 'con': 1, 'pusieron': 1, 'Reina': 1, 'C\xc3\xb3mo': 1}
 
     """
-    content = re.split('[\W]+', self.content)
+    """[\W]+"""
+    content = re.split('[.,\s]', self.content)
     dictionary = {}
     for word in content:
       if(word != ''):     
